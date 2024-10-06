aad30d403f370b4fe0bb5e0cdb31447637e679da
caleb-m0 <cmarroquinq@gmail.com>
2024-10-06 18:52:41 -0500

    cambio de js

--- sept/sept/main.js
+++ sept/sept/main.js
@@ -1,3 +1,5 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
+function mostrarMensaje() {
+        document.getElementById("botonPrincipal").style.display = "none"; // Oculta el primer botón
+        document.getElementById("mensaje").style.display = "block"; // Muestra el mensaje
+        document.getElementById("botonSecundario").style.display = "block"; // Muestra el segundo botón
+    }
